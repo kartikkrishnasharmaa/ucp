145292ba40827ead6ff1882474559be47dbc7c6f
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2024-11-09 12:45:25 +0530

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,4 @@
 module.exports = {
-    siteUrl: 'https://unstopcomputer.vercel.app',
     generateRobotsTxt: true, // (optional)
     sitemapSize: 10000, // (optional)
     // Add any additional configuration here
